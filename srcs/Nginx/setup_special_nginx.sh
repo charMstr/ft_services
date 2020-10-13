b7db4a98624b5d8ab94a0e80ac8931f3a30ab46d
Charlie ARMSTRONG <charmstr@e2r7p10.42.fr>
2020-10-13 17:57:18 +0200

    in the TRY folder in NGINX/ new dockerfile much cleanergit status

--- srcs/Nginx/setup_special_nginx.sh
+++ srcs/Nginx/setup_special_nginx.sh
@@ -8,12 +8,10 @@ eval $(minikube docker-env)
 
 docker build --rm -t my-nginx .
 
-#enable the ingress controller.
-minikube addons enable ingress
-
 #enable the dashboard
 minikube addons dashboard
 
+docker run --rm -it my-nginx
 ######################################################################
 # FIRST SETUP WHILE RUNNIG POD OBJECTS INSTEAD OF DEPLOYMENTS
 ######################################################################
@@ -26,34 +24,3 @@ minikube addons dashboard
 #	access the webpage straight up.
 #minikube service nginx-pod
 
-
-######################################################################
-# THINGS I HAD TO CHANGE TO MAKE IT USABLE IN A YAML FILE
-######################################################################
-
-#	no more need to use the --attach=false -i --tty since in the Dockerfile itself
-#	we created an infinite loop process.
-#kubectl run --generator=run-pod/v1 nginx-pod --image=my-nginx --image-pull-policy=Never --port=80
-#kubectl apply -f Nginx-deployment.yaml
-
-#	now we dont run a pod. but creat a deployment object.
-#kubectl expose deployment.apps/nginx-deployment --type=NodePort
-
-#kubectl expose deployment.apps/nginx-deployment --type=NodePort
-
-#If you want to access the website straight away.
-#minikube service nginx-deployment
-
-######################################################################
-# THINGS I HAD TO CHANGE TO MAKE IT PROPER.
-######################################################################
-
-kubectl apply -f Nginx-deployment.yaml
-
-#now no more need to give the --type=NodePort, the ingress will do the job
-# the default type will be clusterIP, Ip internal to the cluster.
-kubectl apply -f Nginx-service.yaml
-
-kubectl apply -f Nginx-ingress.yaml
-
-open http://`minikube ip`
