018b4a20e784fbd568c1aec0d2eaf33de9063056
charMstr <charles.armstrelli@gmail.com>
2020-10-22 15:20:41 +0200

    new things done, need to addapt the Nginx server folder's files. the configuration of openssl is in progress.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 WORK_DIR=`pwd`
 
-#START THE MINIKUBE WITH CORRECT VM AND 3 CPUS
-minikube start --vm-driver=virtualbox --disk-size=10g --cpus=3 --memory=2448
+#detecting Operating system.
+CURRENT_OS=`uname -s`
 
-#enable the ingress controller.
-minikube addons enable ingress
+#starting minikube, either on Linux or Mac osX
+echo "starting Minikube on $CURRENT_OS"
+if [ $CURRENT_OS = "Linux" ]
+then
+	minikube start --vm-driver=docker
+elif [ $CURRENT_OS = "Darwin" ]; then
+	minikube start --vm-driver=virtualbox --disk-size=10g --cpus=3 --memory=2448
+else
+	echo "Error: current OS not planned in this script to start minikube"
+	return		
+fi
+
+return
 
 #enable the dashboard
 minikube addons enable dashboard
+#enable the ingress controller.
+minikube addons enable ingress
+
 
 #MAKE THE DOCKERD AVAILABLE FROM WITHIN THE MINIKUBE
 eval $(minikube docker-env)
