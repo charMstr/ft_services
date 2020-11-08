4dc56e445cecee23e5a8cba210eaadbd4d40024c
charMstr <charles.armstrelli@gmail.com>
2020-11-08 23:43:42 +0100

    modified the ftps section, more specifically the bootsrap.sh script, so that we only get the proper log messages and not the initial configuration of the container

--- srcs/FTPS/srcs/bootstrap.sh
+++ srcs/FTPS/srcs/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # creat a ssl key pair for our ftps server.
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/C=NL/ST=Noord-Holland/O=Codam/CN=Peer de Bakker' -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/C=NL/ST=Noord-Holland/O=Codam/CN=Peer de Bakker' -keyout /etc/ssl/private/vsftpd.key -out /etc/ssl/certs/vsftpd.crt &>/dev/null
 
 #add a user named "user", its password is "password"
-adduser -D $__FTP_USER__ && echo "$__FTP_USER__:$__FTP_PASSWORD__" | chpasswd
+adduser -D $__FTP_USER__ && echo "$__FTP_USER__:$__FTP_PASSWORD__" | chpasswd &>/dev/null
 
 #creat file of chrooted users, and add user to it
-echo "$__FTP_USER__" > /etc/vsftpd/chroot.list
+echo "$__FTP_USER__" >/etc/vsftpd/chroot.list
 
 # make the logs available to docker:
 # creat the log file before we tail it.
